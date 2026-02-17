a50ce3d527e7823931dbeb750e16c90a073398cf
sriharini10 <sriharini21102004@gmail.com>
2026-02-17 23:33:06 +0530

    commit done

---
+++ Literals/Literalseg.java
@@ -0,0 +1,19 @@
+class Literalseg
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter Your Name:");
+		System.out.println("Aaa");
+		System.out.print("Enter Your Age:");
+		System.out.println(20);
+		System.out.print("Enter Your Height:");
+		System.out.println(5.5);
+		System.out.print("Enter Your Gender:");
+		System.out.println('M');
+		System.out.print("You Are Married:");
+		System.out.println(true);
+		System.out.print("you have child:");
+		System.out.println(false);
+		
+	}
+}
