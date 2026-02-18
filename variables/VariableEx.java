429729e6cf67857abd96803eaac8c21cf30bfc4d
sriharini10 <sriharini21102004@gmail.com>
2026-02-19 00:28:47 +0530

    Added variable folder with Java programs

---
+++ variables/VariableEx.java
@@ -0,0 +1,23 @@
+class VariableEx 
+{
+	public static void main(String[] args) 
+	{
+		String heroine1="samantha";
+		int age1=35;
+		
+		String heroine2="tamana";
+		int age2=33;
+		
+		String heroine3="nayanthara";
+		int age3=45;
+		
+		System.out.println(heroine1);
+		System.out.println(age1);
+		
+		System.out.println(heroine2);
+		System.out.println(age2);
+		
+		System.out.println(heroine3);
+		System.out.println(age3);
+	}
+}
