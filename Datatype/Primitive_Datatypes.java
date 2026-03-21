31587a4efd6a136bb65dcfadf66145d27e621fb3
sriharini10 <sriharini21102004@gmail.com>
2026-03-21 23:54:40 +0530

    Added folder

---
+++ Datatype/Primitive_Datatypes.java
@@ -0,0 +1,30 @@
+class Primitive_Datatypes 
+{
+	public static void main(String[] args) 
+	{
+		String name="Abbas";
+		System.out.println("Student name is: "+name);
+		
+		byte age=21;
+		System.out.println("Student age is: "+age);
+		
+		short marks=450;
+		System.out.println("Student mark is: "+marks);
+		
+		int salary=50000;
+		System.out.println("Student salary is: "+salary);
+		
+		long phNo=9876543210L;
+		System.out.println("Student phone number is: "+phNo);
+		
+		float height=5.5f;
+		System.out.println("Student height is: "+height);
+		
+		double cgpa=7.12345;
+		System.out.println("Student cgpa is: "+cgpa);
+		
+		char gender='m';
+		System.out.println("Student gender is: "+gender);
+		
+	}
+}
